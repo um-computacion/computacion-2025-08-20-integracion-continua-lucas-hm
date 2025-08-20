46492497feeb164df8598a3409161bb666d0341e
lucas-hm <lucashugomoran12@gmail.com>
2025-08-20 09:53:44 -0300

    terminado

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class TestPrueba(unittest.TestCase):
     def test_suma(self):
         self.assertEqual(suma(5, 8), 13)
     def test_resta(self):
-	self.assertEqual(resta(5, 2), 3)
+	    self.assertEqual(resta(5, 2), 3)
 
 if __name__ == "__main__":
     unittest.main()
