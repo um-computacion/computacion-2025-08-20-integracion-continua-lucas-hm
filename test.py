a393182d25da1d9ee8b901069aa0e43334984051
lucas-hm <lucashugomoran12@gmail.com>
2025-08-20 08:47:11 -0300

    terminado

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import unittest
 
 from main import suma
+from main import resta
 
 class TestPrueba(unittest.TestCase):
 
     def test_suma(self):
         self.assertEqual(suma(5, 8), 13)
+    def test_resta(self):
+	self.assertEqual(resta(5, 2), 3)
 
 if __name__ == "__main__":
     unittest.main()
